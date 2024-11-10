7d3ea971fa927112e578091b189bc8a0e7ff6419
Stefan Van Dyck <vandyck.stefan@gmail.com>
2024-11-10 13:03:54 +0100

    Try fixing issue with aiomqtt dependency

--- main.py
+++ main.py
@@ -39,18 +39,42 @@ CONFIG = {
 }
 
 
-async def listen(mqtt_client: aiomqtt.Client):
-        LOGGER.info("Connected to MQTT broker")
-        LOGGER.debug("Publish switch config")
-        await mqtt_client.publish(DISCOVER_TOPIC, json.dumps(CONFIG), qos=2, retain=False)
-        LOGGER.debug("Publish switch state off")
-        await mqtt_client.publish(STATE_TOPIC, OFF, qos=2, retain=False)
-        LOGGER.debug("Subscribing to %s", COMMAND_TOPIC)
-        await mqtt_client.subscribe(COMMAND_TOPIC)
-        async for message in mqtt_client.messages:
-            value = message.payload.decode("utf-8")
-            LOGGER.debug("Recevied message on %s: %s", message.topic, value)
-            await switch(mqtt_client, value)
+async def mqtt(mqtt_client: aiomqtt.Client):
+    LOGGER.info("Connected to MQTT broker")
+    LOGGER.debug("Publish switch config")
+    await mqtt_client.publish(DISCOVER_TOPIC, json.dumps(CONFIG), qos=2, retain=False)
+    LOGGER.debug("Publish switch state off")
+    await mqtt_client.publish(STATE_TOPIC, OFF, qos=2, retain=False)
+    LOGGER.debug("Subscribing to %s", COMMAND_TOPIC)
+    await mqtt_client.subscribe(COMMAND_TOPIC)
+    async for message in mqtt_client.messages:
+        value = message.payload.decode("utf-8")
+        LOGGER.debug("Recevied message on %s: %s", message.topic, value)
+        await switch(mqtt_client, value)
+
+
+async def pulseaudio(mqtt_client: aiomqtt.Client):
+    pulse = pulsectl_asyncio.PulseAsync('audio-switch')
+    await pulse.connect()
+
+    async with pulsectl_asyncio.PulseAsync('sync-status-monitor') as pulse:
+        LOGGER.info("Listening to PulseAudio sink state changes")
+        async for event in pulse.subscribe_events('sink'):
+            if event.t == "change":
+                event: pulsectl.PulseEventInfo
+                LOGGER.debug("Pulse event: %r", event)
+                sink_info: pulsectl.PulseSinkInfo = await pulse.sink_info(event.index)
+                if sink_info.state == "running":
+                    if scheduled_off is not None:
+                        LOGGER.info("Cancelling scheduled off")
+                        scheduled_off.cancel()
+                        scheduled_off = None
+                    await switch(mqtt_client, ON)
+                elif sink_info.state == "idle" or sink_info.state == "suspended":
+                    if scheduled_off is None:
+                        LOGGER.info("Scheduling off")
+                        scheduled_off = asyncio.create_task(schedule_off(mqtt_client))
+                LOGGER.debug("Sink info: %r", sink_info.state)
 
 
 async def switch(client, value):
@@ -65,46 +89,23 @@ async def switch(client, value):
     else:
         LOGGER.error("Invalid payload %s", value)
 
+
 async def schedule_off(mqtt_client):
     await asyncio.sleep(IDLE_TIMEOUT_SECONDS)
     await switch(mqtt_client, OFF)
 
-async def main():
-    scheduled_off = None
 
+async def main():
     LOGGER.info("Setting up GPIO")
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(PIN, GPIO.OUT)
 
     try:
         async with aiomqtt.Client(MQTT_HOST, MQTT_PORT) as mqtt_client:
-            asyncio.create_task(listen(mqtt_client))
-
-            pulse = pulsectl_asyncio.PulseAsync('my-client-name')
-            await pulse.connect()
-
-            async with pulsectl_asyncio.PulseAsync('sync-status-monitor') as pulse:
-                LOGGER.info("Listening to PulseAudio sink state changes")
-                async for event in pulse.subscribe_events('sink'):
-                    if event.t == "change":
-                        event: pulsectl.PulseEventInfo
-                        LOGGER.debug("Pulse event: %r", event)
-                        sink_info: pulsectl.PulseSinkInfo = await pulse.sink_info(event.index)
-                        if sink_info.state == "running":
-                            if scheduled_off is not None:
-                                LOGGER.info("Cancelling scheduled off")
-                                scheduled_off.cancel()
-                                scheduled_off = None
-                            await switch(mqtt_client, ON)
-                        elif sink_info.state == "idle" or sink_info.state == "suspended":
-                            if scheduled_off is None:
-                                LOGGER.info("Scheduling off")
-                                scheduled_off = asyncio.create_task(schedule_off(mqtt_client))
-                        LOGGER.debug("Sink info: %r", sink_info.state)
+            await asyncio.gather(mqtt(mqtt_client), pulseaudio(mqtt_client))
 
     finally:
         GPIO.cleanup()
-
 
 if __name__ == '__main__':
     event_loop = asyncio.get_event_loop()
